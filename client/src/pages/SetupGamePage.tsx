4f7ee3c7455e42322de212d38ea02f061c27799c
Aria Shi <ariashi15@gmail.com>
2026-01-30 23:43:12 -0600

    updated instructions

--- client/src/pages/SetupGamePage.tsx
+++ client/src/pages/SetupGamePage.tsx
@@ -2,10 +2,14 @@ import { TwoColContainer, InstructionBox, InstructionsHeader } from "./SetupGame
 import { Outlet } from "react-router-dom";
 
 function Instructions() {
-    const steps = [
-        'Step 1', 
-        'Step 2', 
-        'Step 3'
+    const steps = [ 
+        'Create a game or join a game via code.',
+        `Go to Instagram Web > Settings > See more in Accounts Center > Your information and permissions > 
+        Export your information > Create export > Export to device.`,
+        'Inside Customize Information, deselect all information except for followers and following.',
+        'Inside Format, select JSON',
+        'Your information may take a few minutes to export. Download to your computer when ready.',
+        'Unzip the file. Upload following.json to Guess You-Know-Who.'
     ];
     
     return (
