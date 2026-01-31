485ae57775e8aa4cf9f730a44e7ff121f706ba8a
Aria Shi <ariashi15@gmail.com>
2026-01-31 14:37:52 -0600

    code generation and game creation endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,8 @@ import { Server } from "socket.io";
 import multer from "multer";
 import { extractProfileLinks } from "./parser";
 
+// SERVER SETUP
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -19,14 +21,56 @@ server.listen(3001, () => {
   console.log("Backend running on port 3001");
 });
 
-// temporary storage for json files
+// STORAGE
+
+// file storage for json
 const upload = multer({ storage: multer.memoryStorage() });
+
+interface GameSession {
+  player1Profiles?: string[];
+  player2Profiles?: string[];
+  createdAt: number;
+}
+
+// game sessions mapped from game code to session
+const gameSessions = new Map<string, GameSession>();
+
+// clean up expired game sessions
+setInterval(() => {
+  const ONE_HOUR = 60 * 60 * 1000;
+  const now = Date.now();
+
+  for (const [gameCode, session] of gameSessions.entries()) {
+    if (now - session.createdAt > ONE_HOUR) {
+      gameSessions.delete(gameCode);
+      console.log(`Cleaned up game session ${gameCode}`)
+    }
+  }
+}, 30 * 60 * 1000) // run cleanup every 30 minutes
+
+// ENDPOINTS
 
 app.get("/", (req, res) => {
   res.send("Game server is running");
 });
 
-// frontend will send files to this endpoint
+app.get("/game/:gameCode/exists", (req, res) => {
+  const { gameCode } = req.params;
+  const session = gameSessions.get(gameCode);
+
+  return res.json({ exists: !!session })
+})
+
+app.post("/game/create", (req, res) => {
+  const { gameCode } = req.body;
+
+  gameSessions.set(gameCode, { createdAt: Date.now() });
+  console.log(`Created game session ${gameCode}`);
+
+  return res.json({ gameCode, created: true})
+})
+
+// frontend uploads following json files
 app.post("/upload", upload.single("file"), (req, res) => {
     if (!req.file) return res.status(400).send('No file uploaded');
 
